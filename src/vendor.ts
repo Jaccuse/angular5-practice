0c975d57281592c836e83943e769d61c533c7b12
Jaccuse <guck@jack-arnold.com>
2017-11-13 23:06:41 -0700

    added vendor

---
+++ src/vendor.ts
@@ -0,0 +1,14 @@
+// Angular5
+import "@angular/platform-browser";
+import "@angular/platoform-browser-dynamic";
+import "@angular/core";
+import "@angular/common";
+import "@angular/router";
+
+// RxJS
+import "rxjs";
+
+// Bootstrap, stuff ¯\_(ツ)_/¯
+import "jquery";
+import "bootstrap-loader";
+import "font-awesome-webpack2";
