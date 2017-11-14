4bf6bf0d242a27574553598585364c2b83aa5c4e
Jaccuse <guck@jack-arnold.com>
2017-11-14 08:35:35 -0700

    added main component

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "angular5-practice",
+    templateUrl: "./templates/angular5-practice.html"
+})
+
+export class AppComponent {}
